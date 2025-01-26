49b8488379d4ebbe3d2264de79b51e9889cb6558
Rinai_ <2024211885@stu.cqupt.edu.cn>
2025-01-26 17:14:19 +0800

    完善缓存功能“ ”

--- DataBase/Document/dao/Grant.go
+++ DataBase/Document/dao/Grant.go
@@ -21,21 +21,31 @@ func Grant(ctx context.Context, permission models.Permission) error {
 	r := rand.New(source)
 	mul := r.Intn(60) + 60
 
-	//看看有没有
-	res := DB.Db.Model(&models.Permission{}).Where("document_id = ? AND user_id = ?", permission.DocumentId, permission.UserId).First(&exist)
-	if res.Error != nil {
-		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
-			DB.Db.Create(&permission)
-			if permission.Type == true {
-				DB.Rdb.SetEx(ctx, key, "true", time.Duration(mul)*time.Minute)
-			} else {
-				DB.Rdb.SetEx(ctx, key, "false", time.Duration(mul)*time.Minute)
+	//先看看缓存有没有
+	ans, err := DB.Rdb.Get(ctx, key).Result()
+	if err != nil {
+		//没有，则在mysql里面找
+		res := DB.Db.Model(&models.Permission{}).Where("document_id = ? AND user_id = ?", permission.DocumentId, permission.UserId).First(&exist)
+		if res.Error != nil {
+			if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+				DB.Db.Create(&permission)
+				if permission.Type == true {
+					DB.Rdb.SetEx(ctx, key, "true", time.Duration(mul)*time.Minute)
+				} else {
+					DB.Rdb.SetEx(ctx, key, "false", time.Duration(mul)*time.Minute)
+				}
+				Logger.Logger.Debug("Dao: Created")
+				return nil
 			}
-			Logger.Logger.Debug("Dao: Created")
-			return nil
+			Logger.Logger.Error("Dao: Internal Error " + res.Error.Error())
+			return res.Error
 		}
-		Logger.Logger.Error("Dao: Internal Error " + res.Error.Error())
-		return res.Error
+	} else {
+		if ans == "true" {
+			exist.Type = true
+		} else {
+			exist.Type = false
+		}
 	}
 	//如果存在，必须要去除原来的权限
 	DB.Db.Where("document_id = ? AND user_id = ?", permission.DocumentId, permission.UserId).Delete(&permission)
